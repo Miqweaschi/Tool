a42321ed718ed63bdd55342e6d375eb082b4e7f7
Miqweaschi <nazzarotessitore48@gmail.com>
2026-07-16 10:35:26 +0200

    migliorate le scritte di output

--- hash-identifier/hash_identifier.cpp
+++ hash-identifier/hash_identifier.cpp
@@ -18,21 +18,21 @@ int main(int argc, char* argv[]) {
     };
     switch(hash.length()) {
         case 32:
-            std::cout << "Tipo di hash: MD5" << std::endl;
+            std::cout << "Tipo di hash (Potrebbe essere): MD5, questo perchè ha 32 caratteri" << std::endl;
             break;
         case 40:
-            std::cout << "Tipo di hash: SHA-1" << std::endl;
+            std::cout << "Tipo di hash (Potrebbe essere): SHA-1, questo perchè ha 40 caratteri" << std::endl;
             break;
         case 64:
-            std::cout << "Tipo di hash: SHA-256" << std::endl;
+            std::cout << "Tipo di hash (Potrebbe essere): SHA-256, questo perchè ha 64 caratteri" << std::endl;
             break;
         case 128:
-            std::cout << "Tipo di hash: SHA-512" << std::endl;
+            std::cout << "Tipo di hash (Potrebbe essere): SHA-512, questo perchè ha 128 caratteri" << std::endl;
             break;
         default:
             for (int i = 0; i < prefissi.size(); ++i) {
                 if (hash.rfind(prefissi[i].prefisso, 0) == 0) {
-                    std::cout << "Tipo di hash: " << prefissi[i].nome_hash << std::endl;
+                    std::cout << "Tipo di hash: " << prefissi[i].nome_hash << ", questo perchè ha " << prefissi[i].prefisso << " come prefisso" << std::endl;
                     break;
                 }
             }   
