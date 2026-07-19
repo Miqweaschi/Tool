d4ee7c7deba31d1a99767fdf21a109caa11612e1
Miqweaschi <nazzarotessitore48@gmail.com>
2026-07-19 15:44:55 +0200

    modificato il codice togliendo una funzione inutile

--- network-analyzer/network-analyzer.cpp
+++ network-analyzer/network-analyzer.cpp
@@ -11,8 +11,6 @@ int main(int argc, char *argv[])
 	pcap_t *handle; /* Handle di sessione */ 
 	char *dev = argv[1]; /* Il dispositivo su cui effettuare lo sniffing */ 
 	char errbuf[PCAP_ERRBUF_SIZE]; /* Stringa di errore */ 
-	bpf_u_int32 mask; /* La nostra netmask */ 
-	bpf_u_int32 net; /* Il nostro IP */ 
 	struct pcap_pkthdr header; /* L'intestazione che pcap ci fornisce */ 
 	const u_char *packet; /* Il pacchetto effettivo */ 
 
@@ -21,13 +19,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Impossibile trovare il dispositivo predefinito: %s\n", errbuf); 
 		return(2); 
 	} 
-	/* Trova le proprietà per il dispositivo */ 
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) { 
-		fprintf(stderr, "Impossibile ottenere la netmask per il dispositivo %s: %s\n", dev, errbuf); 
-		net = 0; 
-		mask = 0; 
-	} 
-	/* Apri la sessione in modalità promiscua */ 
+	
+	
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf); 
 	if (handle == NULL) { 
 		fprintf(stderr, "Impossibile aprire il dispositivo %s: %s\n", dev, errbuf); 
